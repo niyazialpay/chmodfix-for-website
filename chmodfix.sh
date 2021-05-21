e52009f2695c375e199c3a2ab83df087664da84e
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2021-05-21 21:52:37 +0300

    Update chmodfix.sh

--- chmodfix.sh
+++ chmodfix.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Muhammed Niyazi ALPAY
 # https://niyazialpay.com
-directory=`pwd`
-chmod 755 $(find $directory -type d)
-chmod 644 $(find $directory -type f)
+chmod 755 $(find . -type d)
+chmod 644 $(find . -type f)
